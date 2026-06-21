3c8c3f31021b2b2ef01affa645d3092ae3c6234f
Ezequiel <zapata.ezequiel@gmail.com>
2026-06-21 17:25:57 -0300

    feat: read GitHub username from CLI arguments

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 console.log("Github Activity CLI");
+const username = process.argv[2];
+
+if(!username) {
+    console.error("Please provide a username as an argument");
+    console.log("Usage: npm run dev -- <username>");
+    process.exit(1);
+}
+console.log(`Username: ${username}`);
