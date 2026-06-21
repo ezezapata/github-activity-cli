398d55a84303cc9f56ded90a739b673c99edea94
Ezequiel <zapata.ezequiel@gmail.com>
2026-06-21 17:47:46 -0300

    feat: fetching raw data from GitHub API

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-console.log("Github Activity CLI");
-const username = process.argv[2];
+async function getGithubActivity(username: string) {
+    const url = `https://api.github.com/users/${username}/events`;
+    const response = await fetch(url);
+    const data = await response.json();
 
-if(!username) {
-    console.error("Please provide a username as an argument");
-    console.log("Usage: npm run dev -- <username>");
-    process.exit(1);
+    return data;
 }
-console.log(`Username: ${username}`);
+
+async function main() {
+    const username = process.argv[2];
+
+    if(!username) {
+        console.error("Please provide a username as an argument");
+        console.log("Usage: npm run dev <username>");
+        process.exit(1);
+    }
+
+    console.log("Github Activity CLI");
+    console.log(`Username: ${username}`);
+
+    try {
+        const data = await getGithubActivity(username);
+        console.log("Recent Activity:");
+        console.log(data);
+    } catch (error) {
+        console.error("Error:", error);
+        process.exit(1);
+    }
+}
+
+
+main();
